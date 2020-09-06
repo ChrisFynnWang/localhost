c29ea0e065c4fadd5579ce0a03819cd4c14dd422
FynnWang <wftop1@126.com>
2020-09-06 22:48:18 +0800

    master

--- src/main/java/com/wangfan/localhost/controller/RedisController.java
+++ src/main/java/com/wangfan/localhost/controller/RedisController.java
@@ -1,9 +1,22 @@
 package com.wangfan.localhost.controller;
 
+import com.wangfan.localhost.service.RedisService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("redis")
 public class RedisController {
+
+    @Autowired
+    private RedisService redisService;
+
+    @GetMapping("lock")
+    public Object lock() {
+        redisService.redisLock();
+        return StringUtils.EMPTY;
+    }
 }
